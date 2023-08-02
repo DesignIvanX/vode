b6808dbc0a0864d9c502f4108019705ffbd04eab
ivandcdesign <ivandcdesign@gmail.com>
2023-08-02 14:10:29 -0400

    vode1.2

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -1,34 +1,34 @@
 import React from "react";
-import {
-  FaRegSmile,
-  FaRegAngry,
-  FaRegSadTear,
-  FaRegLaughWink,
-} from "react-icons/fa";
+import { FaBottleDroplet } from "react-icons/fa6";
+import { FaLessThan } from "react-icons/fa";
+import { MdWaterDrop } from "react-icons/md";
+import { VscDebugBreakpointUnsupported } from "react-icons/vsc";
 import styles from "./styles.module.css";
 
 const InfoSection = () => {
   return (
     <div className={styles["info-container"]}>
       <div className={styles["info-text"]}>
-        <h2>Our Company Was Founded in 1903</h2>
+        <h2 className={styles["info-text--h2"]}>
+          Our company was founded in 1975
+        </h2>
       </div>
       <div className={styles["emoji-group"]}>
         <div className={styles["emoji-item"]}>
-          <FaRegSmile className={styles["emoji"]} />
-          <p>Emotion 1 - Information here</p>
+          <FaBottleDroplet className={styles["emoji"]} />
+          <p>100% Pura</p>
         </div>
         <div className={styles["emoji-item"]}>
-          <FaRegAngry className={styles["emoji"]} />
-          <p>Emotion 2 - Information here</p>
+          <MdWaterDrop className={styles["emoji"]} />
+          <p>Mejor salud e hidratacion</p>
         </div>
         <div className={styles["emoji-item"]}>
-          <FaRegSadTear className={styles["emoji"]} />
-          <p>Emotion 3 - Information here</p>
+          <FaLessThan className={styles["emoji"]} />
+          <p>Menos sodio</p>
         </div>
         <div className={styles["emoji-item"]}>
-          <FaRegLaughWink className="emoji" />
-          <p>Emotion 4 - Information here</p>
+          <VscDebugBreakpointUnsupported className={styles["emoji"]} />
+          <p>Soporte 24/7</p>
         </div>
       </div>
     </div>
