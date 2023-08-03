18a8ff7c8eb78b9bb2a976ec14e2adcc5520b6ad
ivandcdesign <ivandcdesign@gmail.com>
2023-08-03 10:29:41 -0400

    vode1.3

--- src/components/Information/index.js
+++ src/components/Information/index.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useTypewriter, Cursor } from "react-simple-typewriter";
 import { FaBottleDroplet } from "react-icons/fa6";
 import { FaLessThan } from "react-icons/fa";
 import { MdWaterDrop } from "react-icons/md";
-import { VscDebugBreakpointUnsupported } from "react-icons/vsc";
 import styles from "./styles.module.css";
 
 const InfoSection = () => {
+  const [text] = useTypewriter({
+    words: [
+      "A veces lo mas productivo que puedes hacer es relajarte.",
+      "Nuestra compañia se fundo en 2023.",
+      "Si hay magia en el planeta, esta contenida en el agua.",
+      "Vode, conecta con tu esencia.",
+      "100% pura y mineral.",
+    ],
+    loop: {},
+  });
   return (
     <div className={styles["info-container"]}>
       <div className={styles["info-text"]}>
         <h2 className={styles["info-text--h2"]}>
-          Our company was founded in 1975
+          {text} <Cursor />
         </h2>
       </div>
       <div className={styles["emoji-group"]}>
@@ -25,10 +35,6 @@ const InfoSection = () => {
         <div className={styles["emoji-item"]}>
           <FaLessThan className={styles["emoji"]} />
           <p>Menos sodio</p>
-        </div>
-        <div className={styles["emoji-item"]}>
-          <VscDebugBreakpointUnsupported className={styles["emoji"]} />
-          <p>Soporte 24/7</p>
         </div>
       </div>
     </div>
