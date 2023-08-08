c239e3ee74b08fcbc75010098999c0a52ea4fa87
ivandcdesign <ivandcdesign@gmail.com>
2023-08-08 12:52:05 -0400

    vode1.5

--- src/components/Canva/index.js
+++ src/components/Canva/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import Navegation from "../Navegation/index";
 import Loading from "../Loading/index";
 import Footer from "../Footer/index";
-import WhatsApp from "../WhatsApp/index";
+import Logo from "../../static/Logo.jpg";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
 
 const Canva = ({ children }) => {
   return (
@@ -11,7 +12,14 @@ const Canva = ({ children }) => {
       {children}
       <Loading />
       <Footer />
-      <WhatsApp number="0961154054" />
+      <FloatingWhatsApp
+        phoneNumber="0961154054"
+        chatMessage="Hola 🤝! Queremos cuidar tu salud"
+        placeholder="Escribir un mensaje"
+        accountName="VODE"
+        avatar={Logo}
+        statusMessage="Empresa de agua embotellada"
+      />
     </>
   );
 };
