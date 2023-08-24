1145ba9fe928075e6ad6251caf4eb299f149e656
ivandcdesign <ivandcdesign@gmail.com>
2023-08-24 09:21:53 -0400

    v1.7

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { AiFillInstagram, AiOutlineWhatsApp } from "react-icons/ai";
-import { FaLocationDot, FaClock } from "react-icons/fa6";
 import styles from "./styles.module.css";
 
 const Contact = () => {
   return (
-    <section className={styles["contact"]}>
+    <section id="contact" className={styles["contact"]}>
       <div className={styles["contact-content"]}>
         <div className={styles["contact-content-contact"]}>
           <h2 className={styles["contact-content-contact--h2"]}>CONTACTANOS</h2>
@@ -16,14 +15,14 @@ const Contact = () => {
             Horarios de atencion: 07:00hs a 17:00hs
           </p>
           <div className={styles["contact-content-contact-icon"]}>
+            <a className="btn-primary" href="/">
+              Contacto
+            </a>
             <a
               className={styles["contact-content-contact-icon--a"]}
               href="https://www.xn--diseowebmurcia1-1qb.es/crear-pagina-contacto-de-web/"
             >
               <AiFillInstagram />
-            </a>
-            <a className={styles["contact-content-contact-icon--a"]} href="/">
-              <AiOutlineWhatsApp />
             </a>
           </div>
         </div>
